5495d8d5a48ec54512c0299ca83c92e963aa93bc
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-10-19 11:20:00 +0200

    simplify function

--- protostar/testing/test_case_runners/estimate_fee.py
+++ protostar/testing/test_case_runners/estimate_fee.py
@@ -10,32 +10,22 @@ def estimate_fee(execution_resources) -> Optional[float]:
         "pedersen_builtin": 0.4,
     }
 
-    costs: list[tuple[str, float]] = [
-        (
-            "n_steps",
-            getattr(execution_resources.n_steps, "value", 0) * weights["n_steps"],
-        )
+    costs: list[float] = [
+        getattr(execution_resources.n_steps, "value", 0) * weights["n_steps"],
     ]
     range_check_builtin = execution_resources.builtin_name_to_count_map.get(
         "range_check_builtin"
     )
     if range_check_builtin:
         costs.append(
-            (
-                "range_check_builtin",
-                getattr(range_check_builtin, "value", 0)
-                * weights["range_check_builtin"],
-            )
+            getattr(range_check_builtin, "value", 0) * weights["range_check_builtin"],
         )
     pedersen_builtin = execution_resources.builtin_name_to_count_map.get(
         "pedersen_builtin"
     )
     if pedersen_builtin:
         costs.append(
-            (
-                "pedersen_builtin",
-                getattr(pedersen_builtin, "value", 0) * weights["pedersen_builtin"],
-            )
+            getattr(pedersen_builtin, "value", 0) * weights["pedersen_builtin"],
         )
-
-    return sorted(costs, key=lambda item: item[1])[-1][1]
+    costs.sort(reverse=True)
+    return costs[0]
